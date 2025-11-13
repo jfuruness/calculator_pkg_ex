630eef0755e043f028e5afe606cd29ed135b16da
Justin Furuness <jfuruness@gmail.com>
2025-11-13 11:32:14 -0500

    basic cpp ex

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-TODO
+rename inner dir to src
+create another dir inside that dir called calculator_pkg_ex
+move everything into there
+create another dir called cpp
---
+++ src/calculator_pkg_ex/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+from .calculator import Calculator
+from ._core import __doc__, __version__, add, subtract
+
+__all__ = ["Calculator", "__doc__", "__version__", "add", "subtract"]
