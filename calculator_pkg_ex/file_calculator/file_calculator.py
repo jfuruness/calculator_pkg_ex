599614e9385d6ca970e61a6d2576016aa570f448
Justin Furuness <jfuruness@gmail.com>
2025-11-13 13:36:46 -0500

    almost done!

--- calculator_pkg_ex/file_calculator/file_calculator.py
+++ calculator_pkg_ex/file_calculator/file_calculator.py
@@ -2,13 +2,21 @@
 from pathlib import Path
 
 # pip installed
+from tqdm import tqdm
 
 # our package
 # from ..calculator import Calculator
 from calculator_pkg_ex.calculator import Calculator
 
+
 class FileCalculator(Calculator):
-    def sum_file(self, path=None):
+    def sum_file(self, path=None) -> int:
         if path is None:
             path = Path(__file__).parent / "nums.csv"
-        raise NotImplementedError
+        with tqdm(total=100_000_000, desc="summing file") as pbar:
+            total = 0
+            with path.open() as f:
+                for line in f:
+                    total += int(line)
+                    pbar.update()
+            return total
--- scripts/build_file.py
+++
@@ -1,4 +0,0 @@
-from tqdm import trange
-with open("nums.csv", "w") as f:
-    for i in trange(10_000_000):
-        f.write(f"{i}\n")
