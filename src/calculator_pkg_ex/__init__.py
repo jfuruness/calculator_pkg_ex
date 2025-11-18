4dfd3dc9076808119384fa6ca3b7fdd3172b12ac
Justin Furuness <jfuruness@gmail.com>
2025-11-18 04:17:01 -0500

    working

--- src/calculator_pkg_ex/__init__.py
+++ src/calculator_pkg_ex/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from .calculator import Calculator
-from ._core import __doc__, __version__, CPPFileCalculator
+from ._core import __version__, CPPFileCalculator
+from ._core import __doc__ as _core_doc
 
-__all__ = ["Calculator", "CPPFileCalculator", "__doc__", "__version__"]
+__all__ = ["Calculator", "CPPFileCalculator", "__version__"]
+__doc__ = _core_doc
